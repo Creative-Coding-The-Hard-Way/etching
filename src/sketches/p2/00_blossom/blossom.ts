5ee33b286e4146ab4ac4d1b3a8796b2443f968b5
Bradley Lyman <lyman.brad3211@gmail.com>
2024-03-24 18:59:14 -0700

    add the plant sketch

---
+++ src/sketches/p2/00_blossom/blossom.ts
@@ -0,0 +1,119 @@
+import P5 from "p5";
+import { pushpop } from "~src/lib/p5_state";
+
+export interface PetalParams {
+  petal_color: P5.Color;
+}
+
+/**
+ * Draws a single petal.
+ *
+ * Petals are 150 pixels tall starting from y=0 and ending at y = 150.
+ * Orientation and size can be controlled by calling p5.rotate/translate/scale.
+ */
+export function petal(p5: P5, params: PetalParams) {
+  const y_start = 10;
+  const y_end = 150;
+  const size = 150;
+
+  const c1 = {
+    x: p5.random(70, 90),
+    y: p5.random(30, 60),
+  };
+  const c2 = {
+    x: p5.random(c1.x / 10, c1.x / 5),
+    y: p5.random(65, 120),
+  };
+
+  // fill in the petal
+  pushpop(p5, () => {
+    p5.fill(params.petal_color);
+    p5.strokeWeight(3);
+    p5.bezier(0, y_start, c1.x, c1.y, c2.x, c2.y, 0, y_end);
+    p5.bezier(0, y_start, -c1.x, c1.y, -c2.x, c2.y, 0, y_end);
+  });
+
+  // Detail Lines
+  pushpop(p5, () => {
+    p5.noFill();
+    p5.strokeWeight(1);
+
+    // center line
+    p5.bezier(
+      0,
+      y_start + p5.random(size / 4),
+      p5.random(-5, 5),
+      y_start + size * 0.3,
+      p5.random(-5, 5),
+      y_start + size * 0.6,
+      0,
+      y_end - p5.random(size / 4)
+    );
+
+    // petal crease lines
+    const lines = p5.random(2, 4);
+    for (let i = 2; i <= lines; i++) {
+      const y_start_offset = p5.random(size / 4);
+      const y_end_offset = p5.random(size / 4);
+      p5.bezier(
+        p5.random(0, 10),
+        y_start + y_start_offset,
+        c1.x / i,
+        c1.y,
+        c2.x / i,
+        c2.y,
+        p5.random(0, 10),
+        y_end - y_end_offset
+      );
+      p5.bezier(
+        -p5.random(0, 10),
+        y_start + y_start_offset,
+        -c1.x / i,
+        c1.y,
+        -c2.x / i,
+        c2.y,
+        -p5.random(0, 10),
+        y_end - y_end_offset
+      );
+    }
+  });
+}
+
+export interface BlossomParams {
+  x: number;
+  y: number;
+  size: number;
+  stroke_color: P5.Color;
+  petal_color: P5.Color;
+  petal_count?: number;
+}
+
+/**
+ * Draws a procedurally-generated flower blossom.
+ */
+export function blossom(p5: P5, params: BlossomParams) {
+  const { x, y, size } = params;
+  pushpop(p5, () => {
+    p5.translate(x, y);
+    const scale = size / 300;
+    p5.scale(scale, scale);
+    p5.stroke(params.stroke_color);
+
+    const petals = params.petal_count || p5.ceil(p5.random(4, 9));
+    const step = p5.TWO_PI / petals;
+    const offset = p5.random(p5.PI * 0.5);
+    for (let i = 0; i < petals; i++) {
+      pushpop(p5, () => {
+        p5.rotate(offset + step * i + p5.random(-step / 4, step / 4));
+        petal(p5, { petal_color: params.petal_color });
+      });
+    }
+
+    // Draw the center of the blossom.
+    pushpop(p5, () => {
+      p5.noStroke();
+      p5.fill(params.stroke_color);
+      p5.circle(0, 0, 40);
+    });
+  });
+}
---
+++ src/sketches/p2/01_plant/index.ts
@@ -0,0 +1,137 @@
+import { Page } from "~/src/lib/page";
+import { pushpop } from "~/src/lib/p5_state";
+import { Sketch } from "~/src/lib/sketch";
+import { blossom } from "~/src/sketches/p2/00_blossom/blossom";
+import P5 from "p5";
+
+interface PlantParams {
+  max_level: number;
+  stem_color: P5.Color;
+  ink_color: P5.Color;
+  max_branches: number;
+  petal_color: P5.Color;
+}
+
+function plant(p5: P5, level: number, params: PlantParams) {
+  if (level >= params.max_level) {
+    blossom(p5, {
+      x: 0,
+      y: 0,
+      size: p5.random(250, 310),
+      stroke_color: params.ink_color,
+      petal_color: params.petal_color,
+      petal_count: 6,
+    });
+    return;
+  }
+
+  pushpop(p5, () => {
+    const stem_length = 50;
+    const bottom_width = 3;
+    const top_width = 2;
+
+    // draw the stem
+    p5.noStroke();
+    p5.fill(params.stem_color);
+    p5.beginShape();
+    p5.vertex(bottom_width, 0);
+    p5.vertex(top_width, stem_length);
+    p5.vertex(-top_width, stem_length);
+    p5.vertex(-bottom_width, 0);
+    p5.endShape();
+
+    // draw the stem outline
+    p5.stroke(params.ink_color);
+    p5.strokeWeight(3);
+    p5.line(3, 0, 2, stem_length);
+    p5.line(-3, 0, -2, stem_length);
+
+    if (level < 3 && params.max_branches > 0) {
+      if (p5.random() > 0.75) {
+        params.max_branches -= 1;
+        pushpop(p5, () => {
+          p5.translate(0, stem_length);
+          p5.rotate(p5.PI * p5.random(-0.25, 0.25));
+          p5.scale(0.9, 0.9);
+          plant(p5, level + 1, params);
+        });
+      }
+    }
+
+    // draw the next stem segment
+    pushpop(p5, () => {
+      p5.translate(0, stem_length);
+      p5.rotate(p5.PI * p5.random(-0.15, 0.15));
+      p5.scale(0.9, 0.9);
+      plant(p5, level + 1, params);
+    });
+  });
+}
+
+new Page("Plant", (p5: P5) => {
+  const sketch = new Sketch(p5, {
+    width: 800,
+    height: 1000,
+    margin: 50,
+  });
+  const { w, h, scene } = sketch.layout;
+
+  p5.colorMode(p5.HSL);
+  const flower_colors = [
+    p5.color(51, 75, 51),
+    p5.color(212, 30, 60),
+    p5.color(289, 15, 55),
+    p5.color(332, 30, 65),
+  ];
+
+  p5.preload = () => sketch.assets.load_special_elite();
+  p5.setup = () => p5.createCanvas(w, h);
+
+  p5.draw = () => {
+    p5.background(sketch.palette.old_paper());
+
+    const horizon = scene.bottom - scene.h * 0.3;
+
+    // draw a flower
+    const offset = scene.w * 0.25;
+    const flower_count = p5.floor(p5.random(1, 8));
+    for (let i = 0; i < flower_count; i++) {
+      pushpop(p5, () => {
+        p5.translate(
+          p5.random(scene.left + offset, scene.right - offset),
+          horizon
+        );
+        p5.rotate(p5.PI * p5.random(-0.1, 0.1));
+        const s = p5.random(0.8, 1.1);
+        p5.scale(s, -s);
+        plant(p5, 0, {
+          max_level: 7,
+          stem_color: sketch.palette.old_paper(),
+          ink_color: sketch.palette.gunmetal(),
+          max_branches: 4,
+          petal_color: p5.random(flower_colors),
+        });
+      });
+    }
+
+    // draw the ground
+    pushpop(p5, () => {
+      p5.strokeWeight(3);
+      p5.stroke(sketch.palette.gunmetal());
+      p5.line(scene.left, horizon, scene.right, horizon);
+    });
+
+    // draw the seed
+    pushpop(p5, () => {
+      p5.fill(sketch.palette.old_paper());
+      sketch.paint_margins();
+      p5.fill(sketch.palette.gunmetal());
+      p5.textFont(sketch.assets.load_special_elite());
+      sketch.draw_seed();
+    });
+
+    p5.noLoop();
+  };
+});
+
+export {};
